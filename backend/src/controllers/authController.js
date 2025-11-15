da65fbf71645bf9d8222fd32114f462a4d1cf00f
tomhuyquang@gmail.com <tomhuyquang@gmail.com>
2025-11-15 20:27:13 +0700

    done sigin api

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,11 @@
 import bcrypt from "bcrypt";
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import Session from "../models/Session.js";
+
+const ACCESS_TOKEN_TTL = "30m";
+const REFRESH_TOKEN_TTL = 14 * 24 * 1000 * 60 * 60; // 14 ngày
 
 export const signUp = async (req, res) => {
   try {
@@ -28,6 +34,67 @@ export const signUp = async (req, res) => {
 
     // return
     return res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const signIn = async (req, res) => {
+  try {
+    // lấy input từ res body
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ message: "Username and password are required" });
+    }
+    // lấy hashedPassword từ db
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "Username hoặc password không đúng" });
+    }
+
+    const passwordCorrect = await bcrypt.compare(password, user.hashedPassword);
+    if (!passwordCorrect) {
+      return res
+        .status(401)
+        .json({ message: "Username hoặc password không đúng" });
+    }
+
+    // khớp thì tạo access token
+    const accessToken = jwt.sign(
+      { userId: user._id },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: ACCESS_TOKEN_TTL }
+    );
+
+    // tạo refresh token
+    const refreshToken = crypto.randomBytes(64).toString("hex");
+
+    // tạo session mới để lưu refresh token
+    await Session.create({
+      userId: user._id,
+      refreshToken,
+      expiresAt: new Date(Date.now() + REFRESH_TOKEN_TTL),
+    });
+
+    // trả refresh token về cookie
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: REFRESH_TOKEN_TTL,
+    });
+
+    // trả access token trong res
+    return res.status(200).json({
+      message: `User ${username} signed in successfully`,
+      accessToken,
+    });
   } catch (error) {
     return res.status(500).json({ message: "Internal server error" });
   }
